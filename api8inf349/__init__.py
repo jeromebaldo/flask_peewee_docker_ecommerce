8559e3b7d8c7841f8a39bbc32718ee3f00b20f47
baldojerome <99471537+jeromebaldo@users.noreply.github.com>
2023-04-07 17:44:01 -0400

    push render template

--- api8inf349/__init__.py
+++ api8inf349/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify, redirect, url_for, request
+from flask import Flask, jsonify, redirect, url_for, request, render_template
 from peewee import *
-import os
-import requests
 from api8inf349.models.models import Product, init_app
 from api8inf349.services.services_ext import API_Ext_Services
 from api8inf349.services.services_int import API_Inter_Product_Services
+
 
 def create_app(initial_config=None):
     app = Flask("api8inf349")
@@ -19,8 +18,10 @@ def create_app(initial_config=None):
     
     @app.route('/', methods=['GET'])
     def get_products():
-        product_list = []
-        product_list = API_Inter_Product_Services.list_Total_Product()
-        return jsonify({"products": product_list}), 200
+
+        products = []
+        products = API_Inter_Product_Services.list_Total_Product()
+
+        return render_template('product.html', products=products), 200
     
     return app
